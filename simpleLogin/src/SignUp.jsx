5e1457b197dfe9d5e2ca9803df22a3535f15664c
siddh113 <siddharthketan@gmail.com>
2024-08-22 16:27:53 +0530

    Complete sign up form ith mongo DB

--- simpleLogin/src/SignUp.jsx
+++ simpleLogin/src/SignUp.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import {useNavigate} from "react-router-dom"
 
 const signUp = () => {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/register', {name, email, password})
+        .then(result => {console.log(result)
+            navigate('./login')
+        })
+        .catch(err => console.log(err))
+    }
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
         <div className="bg-white p-3 rounded w-25">
             <h2>Register</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor='email'>
                         <strong>Name</strong>
                     </label>
                     <input type='text' placeholder='Enter Name' autoComplete='off'
-                    name='email' className='form-control rounded-0'/>
+                    name='email' className='form-control rounded-0' 
+                    onChange={(e) => setName(e.target.value)}/>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor='email'>
+                        <strong>Email</strong>
+                    </label>
+                    <input type='text' placeholder='Enter Name' autoComplete='off'
+                    name='email' className='form-control rounded-0' 
+                    onChange={(e) => setEmail(e.target.value)}/>
                 </div>
 
                 <div className="mb-3">
@@ -20,7 +46,8 @@ const signUp = () => {
                         <strong>Password</strong>
                     </label>
                     <input type='password' placeholder='Enter Password' autoComplete='off'
-                    name='password' className='form-control rounded-0'/>
+                    name='password' className='form-control rounded-0'
+                    onChange={(e) => setPassword(e.target.value)}/>
                 </div>
 
                 <button type='submit' className="btn btn-successw-100 rounded-0">
